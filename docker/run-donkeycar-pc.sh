1bdf9f4d76af3393443a58bdc01a33e7200dbb64
naisy <takanashi@gclue.jp>
2021-09-28 14:16:43 +0900

    fix DISPLAYNAME

--- docker/run-donkeycar-pc.sh
+++ docker/run-donkeycar-pc.sh
@@ -15,8 +15,8 @@ if [ ! -f $HOST_USER_HOME/$XAUTH_FILE ]; then
     touch $HOST_USER_HOME/$XAUTH_FILE
     chown $HOST_USER:$HOST_USER_GROUP $HOST_USER_HOME/$XAUTH_FILE
     chmod 600 $HOST_USER_HOME/$XAUTH_FILE
-    su $HOST_USER -c "xauth generate :0 . trusted"
-    su $HOST_USER -c "xauth generate :1 . trusted"
+    DISPLAYNAME=`echo $DISPLAY`
+    su $HOST_USER -c "xauth generate $DISPLAYNAME . trusted"
 fi
 
 if [ ! -d "$HOST_MOUNT_PATH" ]; then
